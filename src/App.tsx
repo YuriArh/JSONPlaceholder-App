d5372583021a861d877eb1e79b115a5088a12c7a
Юрий Архипов <arhipow.yuri@gmail.com>
2023-06-01 20:54:34 +0600

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import { useEffect, useState } from "react";
 import { getPosts } from "./action";
 import { useDispatch, useSelector } from "react-redux";
+import { FadeLoader } from "react-spinners";
+import { Container } from "react-bootstrap";
 import PostList from "./components/PostList";
 import storeType from "./types/storeType";
 import Search from "./components/Search";
@@ -20,6 +22,7 @@ function App() {
     dispatch(getPosts(term, page, limit));
   }, [term, page]);
   const posts = useSelector((store: storeType) => store.posts.posts);
+  const loading = useSelector((store: storeType) => store.posts.loading);
 
   const sortedPosts = sort
     ? [...posts].sort((a, b) => {
@@ -32,8 +35,16 @@ function App() {
       <Header />
       <Search term={term} setTerm={setTerm} />
       <Sort setSort={setSort} />
-      {posts ? <PostList posts={sortedPosts} /> : ""}
-      <Pagination page={page} setPage={setPage} limit={limit} total={100} />
+      {!loading ? (
+        <>
+          <PostList posts={sortedPosts} />
+          <Pagination page={page} setPage={setPage} limit={limit} total={100} />
+        </>
+      ) : (
+        <Container className="d-flex justify-content-center mt-5 mb-5">
+          <FadeLoader color="#106cf6" />
+        </Container>
+      )}
     </>
   );
 }
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -3,7 +3,8 @@ import { getComments } from "../action";
 import { useDispatch, useSelector } from "react-redux";
 import storeType from "../types/storeType";
 import { comment } from "../types/storeType";
-import { Card } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+import { FadeLoader } from "react-spinners";
 
 function Comments(props: { id: number }) {
   const dispatch = useDispatch();
@@ -11,21 +12,25 @@ function Comments(props: { id: number }) {
     dispatch(getComments(props.id));
   }, []);
   const comments = useSelector((store: storeType) => store.comments.comments);
-  console.log(comments);
+  const loading = useSelector((store: storeType) => store.comments.loading);
   return (
     <ul>
-      {comments
-        ? comments.map((item: comment) => {
-            return (
-              <Card key={item.id} border="light">
-                <Card.Body>
-                  <Card.Title>{item.email}</Card.Title>
-                  <Card.Body>{item.body}</Card.Body>
-                </Card.Body>
-              </Card>
-            );
-          })
-        : ""}
+      {!loading ? (
+        comments.map((item: comment) => {
+          return (
+            <Card key={item.id} border="light">
+              <Card.Body>
+                <Card.Title>{item.email}</Card.Title>
+                <Card.Body>{item.body}</Card.Body>
+              </Card.Body>
+            </Card>
+          );
+        })
+      ) : (
+        <Container className="d-flex justify-content-center mt-5 mb-5">
+          <FadeLoader color="#106cf6" />
+        </Container>
+      )}
     </ul>
   );
 }
